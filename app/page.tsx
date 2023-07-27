f5ab65e0925af1da4e6dce902ff0fc79aeba9ded
strmzi-git <timo@storny.de>
2023-07-27 15:44:41 +0200

    Removed design carousel temporarily

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
       <Header />
       <MainPage />
       <FeaturesPage />
-      <DesignCarousel />
+      {/* <DesignCarousel /> */}
       <PricingPage />
       {/* <CallToAction /> */}
       {/* <FAQ /> */}
